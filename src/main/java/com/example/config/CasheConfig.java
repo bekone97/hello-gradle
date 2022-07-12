e7bd7ef0a9c6e0d700b14fce7ab64a178b083910
@a.miachyn <a.miachyn@godeltech.com>
2022-07-12 11:40:24 +0300

    Added HazelcastCacheManager to CacheManager

--- src/main/java/com/example/config/CasheConfig.java
+++ src/main/java/com/example/config/CasheConfig.java
@@ -2,14 +2,18 @@ package com.example.config;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
-public class CashConfig {
-
+public class CasheConfig {
 
     @Bean
     public Config hazelCastConfig() {
@@ -22,4 +26,13 @@ public class CashConfig {
 
     }
 
+    @Bean
+    public HazelcastInstance hazelCastInstance(Config hazelCastConfig){
+        return Hazelcast.newHazelcastInstance(hazelCastConfig);
+    }
+
+    @Bean
+    public CacheManager cacheManager(@Qualifier("hazelCastInstance") HazelcastInstance hazelcastInstance) {
+        return new HazelcastCacheManager(hazelcastInstance);
+    }
 }
